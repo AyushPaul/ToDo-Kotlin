eddab45ed8781632c70c9bd8c45833a8cf2cce0e
Ayush Paul <ayushpaul2002@gmail.com>
2021-12-18 12:02:15 +0530

    Added Final Project Commit

---
+++ app/src/main/java/com/example/todoapp/HistoryActivity.kt
@@ -0,0 +1,120 @@
+package com.example.todoapp
+
+import android.graphics.*
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class HistoryActivity : AppCompatActivity() {
+
+    val list = arrayListOf<TodoModel>()
+    var adapter = TodoAdapter(list)
+
+    val db by lazy {
+        AppDatabase.getDatabase(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+        todoRv.apply {
+            layoutManager = LinearLayoutManager(this@HistoryActivity)
+            adapter = this@HistoryActivity.adapter
+        }
+        initSwipe()
+        db.todoDao().findfinishedTask().observe(this, Observer {
+            if (!it.isNullOrEmpty()) {
+                list.clear()
+                list.addAll(it)
+                adapter.notifyDataSetChanged()
+            }else{
+                list.clear()
+                adapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+
+    fun initSwipe() {
+        val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    GlobalScope.launch(Dispatchers.IO) {
+                        db.todoDao().deleteTask(adapter.getItemId(position))
+                    }
+                }
+            }
+
+            override fun onChildDraw(
+                canvas: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    val itemView = viewHolder.itemView
+
+                    val paint = Paint()
+                    val icon: Bitmap
+
+                     if(dX < 0) {
+                        icon = BitmapFactory.decodeResource(resources, R.mipmap.ic_delete_white_png)
+
+                        paint.color = Color.parseColor("#D32F2F")
+
+                        canvas.drawRect(
+                            itemView.right.toFloat() + dX, itemView.top.toFloat(),
+                            itemView.right.toFloat(), itemView.bottom.toFloat(), paint
+                        )
+
+                        canvas.drawBitmap(
+                            icon,
+                            itemView.right.toFloat() - icon.width,
+                            itemView.top.toFloat() + (itemView.bottom.toFloat() - itemView.top.toFloat() - icon.height.toFloat()) / 2,
+                            paint
+                        )
+                    }
+                    viewHolder.itemView.translationX = dX
+
+
+                } else {
+                    super.onChildDraw(
+                        canvas,
+                        recyclerView,
+                        viewHolder,
+                        dX,
+                        dY,
+                        actionState,
+                        isCurrentlyActive
+                    )
+                }
+            }
+
+
+        }
+
+        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(todoRv)
+    }
+}
